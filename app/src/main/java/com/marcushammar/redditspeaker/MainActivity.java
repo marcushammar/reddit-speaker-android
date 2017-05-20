dd2a2ce2313a620a2274169d32eed8824d67b64a
Marcus Hammar <marcus@infium.se>
2017-05-20 16:11:31 +0200

    Use an instance variable for logging

--- app/src/main/java/com/marcushammar/redditspeaker/MainActivity.java
+++ app/src/main/java/com/marcushammar/redditspeaker/MainActivity.java
@@ -2,12 +2,17 @@ package com.marcushammar.redditspeaker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String log = "Log initiated";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView logTextView = (TextView)findViewById(R.id.logTextView);
+        logTextView.setText(log);
     }
 }
